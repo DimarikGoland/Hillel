942902b2f0d2a8a5564bf713e5849fbfab846a37
DimarikGoland <dima160186@icloud.com>
2024-03-17 18:48:29 +0200

    dz3

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"sort"
+)
 
 type Animal struct {
 	runSpeed int
@@ -32,30 +36,112 @@ type Race struct {
 }
 
 func main() {
+	distance := 1000
+	fmt.Printf("Distance: %d\n ", distance)
 	fish, turtle, tiger := CreateTeam()
-	fmt.Println(fish, tiger, turtle)
-}
-func (Animal) WinnerSay() {
+	fishSpeed := flag.Int(fish.name, 1, "Speed F")
+	turtleSpeed := flag.Int(turtle.name, 1, "Speed T")
+	tigerSpeed := flag.Int(tiger.name, 1, "Speed Tig")
 
-}
-func (Animal) LoserSay() {
+	flag.Parse()
 
+	fmt.Printf("Speed of fish: %v\n", *fishSpeed)
+	fmt.Printf("Speed of turtle: %v\n", *turtleSpeed)
+	fmt.Printf("Speed of tiger: %v\n", *tigerSpeed)
+	_, runSpeedF := Race{
+		Distance: distance,
+		fish: Fish{
+			name:      fish.name,
+			fins:      fish.fins,
+			colorSkin: fish.colorSkin,
+		},
+	}.Start(*fishSpeed)
+
+	animalFish := Animal{
+		runSpeed: runSpeedF,
+		name:     fish.name,
+		voice:    "",
+	}
+	_, runSpeedT := Race{
+		Distance: distance,
+		turtle: Turtle{
+			name:      turtle.name,
+			legs:      turtle.legs,
+			colorSkin: turtle.colorSkin,
+		},
+	}.Start(*turtleSpeed)
+	animalTurtle := Animal{
+		runSpeed: runSpeedT,
+		name:     turtle.name,
+		voice:    "",
+	}
+	_, runSpeedTiger := Race{
+		Distance: distance,
+		tiger: Tiger{
+			name:      tiger.name,
+			legs:      tiger.legs,
+			colorSkin: tiger.colorSkin,
+		},
+	}.Start(*tigerSpeed)
+	animalTiger := Animal{
+		runSpeed: runSpeedTiger,
+		name:     tiger.name,
+		voice:    "",
+	}
+	var animals []Animal
+	animals = append(animals, animalFish, animalTurtle, animalTiger)
+	sort.Sort(ByRunSpeed(animals))
+
+	fmt.Printf("%v is win, time off iteration: %d\n ", animals[0].name, animals[0].runSpeed)
+	fmt.Println(Animal{}.WinnerSay())
+	fmt.Printf("Second plays is %v, time off iteration: %d\n ", animals[1].name, animals[1].runSpeed)
+	fmt.Printf("Third plays is %v, time off iteration: %d\n ", animals[2].name, animals[2].runSpeed)
+	fmt.Printf(Animal{}.LoserSay())
+}
+
+func (a Animal) WinnerSay() string {
+	a.voice = "I am best of the best off the best! "
+	return a.voice
+}
+func (a Animal) LoserSay() string {
+	a.voice = "I am win next time! "
+	return a.voice
 }
 func CreateTeam() (Fish, Turtle, Tiger) {
 	fish := Fish{
-		"nemo",
+		"Nemo",
 		8,
 		"grey",
 	}
 	turtle := Turtle{
-		name:      "Tortilla",
-		legs:      4,
-		colorSkin: "black",
+		"Tortilla",
+		4,
+		"black",
 	}
 	tiger := Tiger{
-		name:      "Balu",
-		legs:      4,
-		colorSkin: "Yellow-black",
+		"Balu",
+		4,
+		"Yellow-black",
 	}
 	return fish, turtle, tiger
 }
+
+func (r Race) Start(animal int) (race Race, iter int) {
+	var i int
+
+	for {
+		if r.Distance <= 0 {
+			break
+		}
+		r.Distance = r.Distance - animal
+		i++
+
+	}
+	return race, i
+}
+
+type ByRunSpeed []Animal
+
+func (b ByRunSpeed) Len() int           { return len(b) }
+func (b ByRunSpeed) Less(i, j int) bool { return b[i].runSpeed < b[j].runSpeed }
+func (b ByRunSpeed) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
